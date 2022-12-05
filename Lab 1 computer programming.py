229db22a1857a63eaa7fcd8d33bbfa4cf26fe608
IsaGraceGuthrie <116840075+IsaGraceGuthrie@users.noreply.github.com>
2022-12-04 20:41:15 -0500

    Add files via upload

---
+++ Lab 1 computer programming.py
@@ -0,0 +1,57 @@
+Python 3.10.6 (v3.10.6:9c7b4bd164, Aug  1 2022, 17:13:48) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+#Isa Grace and Ellen
+print("Hello, world")
+Hello, world
+print("Welcome to CPSC 207")
+Welcome to CPSC 207
+print("Hello\nWorld")
+Hello
+World
+print("How\nare\nyou?")
+How
+are
+you?
+print("Today","is","Tuesday")
+Today is Tuesday
+print("Today" + "is" + "Tuesday")
+TodayisTuesday
+print("Hello\n" + "\nWorld")
+Hello
+
+World
+print("123\n45\n6")
+123
+45
+6
+1+2*3
+7
+(1+2)*3
+9
+7/3
+2.3333333333333335
+7//3
+2
+7%3
+1
+22/4
+5.5
+22//4
+5
+22%4
+2
+3**2
+9
+2**3
+8
+1+2*(3**4)
+163
+print("2+2", 2+2)
+2+2 4
+
+print("2+2=", 2+2)
+2+2= 4
+print("2+2=",2+2)
+2+2= 4
+print("50 pounds is", 50*0.453592, "Kilograms")
+50 pounds is 22.6796 Kilograms
